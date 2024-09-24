10a888cdebbb304d802bf59a50f914701b4f581a
NrdnKrmt <nourdinkaroumat@gmail.com>
2024-09-24 16:26:30 +0200

    Change souts

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Bye world!");
+        System.out.println("Hi World!");
+        System.out.println("Bye World!");
     }
 
 }
